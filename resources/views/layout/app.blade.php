300253526c92a50706fd287a7a6c814f03180870
Van Quy <vanquynguyen1604@gmail.com>
2017-10-09 13:30:27 +0700

    Hoan thanh example 1

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+        <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+        <link href="{{asset('fonts/font-awesome.css')}}" rel="stylesheet">
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+        <script src="{{asset('js/jquery.min.js')}}"></script>
+        <script src="{{asset('js/bootstrap.min.js')}}"></script>
+</head>
+<body>
+	@yield('content');
+</body>
+</html>
